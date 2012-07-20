ac0136e7fefc4ced927ed4b494a2e3f9f736d2a6
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2012-07-20 02:53:21 +0000

    authentication and database configuration

---
+++ application/config/database.php
@@ -0,0 +1,40 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+  * There are two ways to define a connection for PostgreSQL:
+  *
+  * 1. Full connection string passed directly to pg_connect()
+  *
+  * string   info
+  *
+  * 2. Connection parameters:
+  *
+  * string   hostname    NULL to use default domain socket
+  * integer  port        NULL to use the default port
+  * string   username
+  * string   password
+  * string   database
+  * boolean  persistent
+  * mixed    ssl         TRUE to require, FALSE to disable, or 'prefer' to negotiate
+  *
+  * @link http://www.postgresql.org/docs/current/static/libpq-connect.html
+  */
+
+return array(
+	'default' => array(
+		'type'       => 'postgresql',
+		'connection' => array(
+			'hostname'   => 'localhost',
+			'username'   => 'sgs',
+			'password'   => '5gSu8z_',
+			'persistent' => FALSE,
+			'database'   => 'sgs',
+		),
+		'primary_key'  => 'id',
+		'schema'       => '',
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'caching'      => FALSE,
+		'profiling'    => TRUE,
+	),
+);
