0ac4aa2e0ce89410740230b6344a9b5c145dc927
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2012-08-08 08:34:34 +0000

    notification settings

---
+++ application/config/notify.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'default_message_type' => 'notice',
+	'view'                 => 'notify/notify',
+	'persistent_messages'	 => FALSE,
+	'translate'            => FALSE,
+	'session_name'			   => 'notify_messages',
+);
+
