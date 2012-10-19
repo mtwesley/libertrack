ba7550c042115b245de2ec1f6c82d243b61e1bf8
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2012-10-19 10:33:37 +0000

    next set of updates

--- application/views/tips.php
+++ application/views/tips.php
@@ -1,20 +1,15 @@
 <div class="details-tips">
-  <div class="details-tips-title">Tips</div>
-  
   <?php foreach ($tips as $field => $tip): ?>
+  <div class="details-tips-title"><?php if ($fields[$field]) echo $fields[$field]; ?> Tips</div>
   <div class="details-tip">
-    <?php if (count($tips) > 1): ?>
-    <strong><?php if ($fields[$field]) echo $fields[$field]; ?>:</strong>
-    <?php endif; ?>
-
     <?php echo $tip; ?>
   </div>
-  <?php endforeach; ?>
 
   <?php if (!$tips): ?>
   <div class="details-no-tips">
     Sorry, no tips found.
   </div>
+  <?php endforeach; ?>
   <?php endif; ?>
 </div>
 
