0d8dd9da96f255242c30db1e791ea27006ea8b60
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2012-11-27 14:11:36 -0500

    adding bug tracker

---
+++ mantis/config_inc.php
@@ -0,0 +1,14 @@
+<?php
+	$g_hostname = 'localhost:5432';
+	$g_db_type = 'pgsql';
+	$g_database_name = 'mantis';
+	$g_db_username = 'sgs';
+	$g_db_password = 's1lvIa';
+  $g_window_title     = 'SGS · LiberFor | Bug Tracker';
+  $g_logo_image     = 'images/liberbugtracker.jpg';
+  $g_favicon_image    = 'images/liberfor_favicon.png';
+  $g_validate_email = OFF;
+  $g_allow_blank_email = ON;
+  $g_enable_email_notification = OFF;
+  $g_allow_signup = OFF;
+?>
