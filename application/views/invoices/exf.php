0121a4813bde14b88804514b1c84ee7943222377
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2013-05-01 13:16:43 +0000

    fix for invoices

--- application/views/invoices/exf.php
+++ application/views/invoices/exf.php
@@ -277,28 +277,20 @@ $options = (array) $options + array(
         <td><?php echo SGS::date($invoice->created_date, SGS::PRETTY_DATE_FORMAT); ?></td>
       </tr>
       <tr>
-        <td rowspan="4" class="label label-left">Address:</td>
+        <td rowspan="2" class="label label-left">Address:</td>
         <td class="desc-left address" rowspan="4"><?php echo SGS::breakify($invoice->operator->address); ?></td>
         <td class="label">Date Due:</td>
         <td><?php echo SGS::date($invoice->due_date, SGS::PRETTY_DATE_FORMAT); ?></td>
       </tr>
       <tr>
-        <td class="label specs_barcode">Shipment Specification Barcode:</td>
-        <td><?php if ($specs_barcode) echo $specs_barcode; ?></td>
-      </tr>
-      <tr>
-        <td class="label specs_number">Shipment Specification Number:</td>
+        <td class="label specs_number">SPEC Number:</td>
         <td><?php if ($specs_number) echo $specs_number; ?></td>
-      </tr>
-      <tr>
-        <td class="label exp_barcode">Export Permit Barcode:</td>
-        <td><?php if ($exp_barcode) echo $exp_barcode; ?></td>
       </tr>
       <tr>
         <td class="label label-left">Telephone:</td>
         <td class="desc-left"><?php echo $operator->phone; ?></td>
-        <td class="label exp_number">Export Permit Number:</td>
-        <td><?php if ($exp_number) echo $exp_number; ?></td>
+        <td class="label specs_barcode">SPEC Barcode:</td>
+        <td><?php if ($specs_barcode) echo $specs_barcode; ?></td>
       </tr>
       <tr>
         <td class="label label-left">E-mail:</td>
