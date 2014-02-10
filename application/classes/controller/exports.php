2124b79b99273156085e5c056e190f48942e34bf
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2014-02-10 13:35:23 +0000

    modifying SPECS check so that short-shipped logs work

--- application/classes/controller/exports.php
+++ application/classes/controller/exports.php
@@ -545,7 +545,7 @@ VALIDATION: $secret";
             ->group_by('barcodes.barcode')
 
             ->having(DB::expr('coalesce(array_agg(distinct "barcode_activity"."activity"::text), \'{}\')'), '@>', DB::expr("array[/*'D',*/'X']"))
-            ->and_having(DB::expr('NOT coalesce(array_agg(distinct "barcode_activity"."activity"::text), \'{}\')'), '@>', DB::expr("array['E', 'O', 'H', 'Y', 'A', 'L', 'Z']"))
+            ->and_having(DB::expr('NOT coalesce(array_agg(distinct "barcode_activity"."activity"::text), \'{}\')'), '&&', DB::expr("array['E', 'O', 'H', 'Y', 'A', 'L', 'Z']"))
 
             ->and_having(DB::expr('array_agg(distinct "exp_documents"."id"::text)'), '=', NULL)
             ->and_having(DB::expr('coalesce(array_agg(distinct "invoices_paid"."id"::text), \'{}\')'), '@>', DB::expr('coalesce(array_agg(distinct "invoices"."id"::text), \'{}\')'))
@@ -630,13 +630,16 @@ VALIDATION: $secret";
             ->group_by('specs_data.id')
             ->group_by('barcodes.barcode')
 
-            ->having(DB::expr('NOT coalesce(array_agg(distinct "barcode_activity"."activity"::text), \'{}\')'), '@>', DB::expr("array['E','O','H','Y','A','L','S','Z']"))
-            ->and_having(DB::expr('NOT coalesce(array_agg(distinct "parent_barcode_activity"."activity"::text), \'{}\')'), '@>', DB::expr("array['D','E','O','H','Y','A','L','S','Z']"))
-            ->and_having(DB::expr('NOT coalesce(array_agg(distinct "children_barcode_activity"."activity"::text), \'{}\')'), '@>', DB::expr("array['D','E','O','H','Y','A','L','S','Z']"))
+            ->having(DB::expr('NOT coalesce(array_agg(distinct "barcode_activity"."activity"::text), \'{}\')'), '&&', DB::expr("array['E','O','H','Y','A','L','Z']"))
+            ->and_having(DB::expr('NOT coalesce(array_agg(distinct "parent_barcode_activity"."activity"::text), \'{}\')'), '&&', DB::expr("array['D','E','O','H','Y','A','L','S','Z']"))
+            ->and_having(DB::expr('NOT coalesce(array_agg(distinct "children_barcode_activity"."activity"::text), \'{}\')'), '&&', DB::expr("array['D','E','O','H','Y','A','L','S','Z']"))
 
             ->and_having(DB::expr('array_agg(distinct "documents"."id"::text)'), '=', NULL)
             ->and_having(DB::expr('array_agg(distinct "related_documents"."id"::text)'), '=', NULL)
-            ->and_having(DB::expr('array_agg(distinct "parent_documents"."id"::text)'), '=', NULL)
+            ->and_having_open()
+                  ->or_having(DB::expr('array_agg(distinct "parent_documents"."id"::text)'), '=', NULL)
+                  ->or_having(DB::expr('coalesce(array_agg(distinct "barcode_activity"."activity"::text), \'{}\')'), '@>', DB::expr("array['S']"))
+            ->and_having_close()
             ->and_having(DB::expr('array_agg(distinct "children_documents"."id"::text)'), '=', NULL)
 
             ->and_having(DB::expr('coalesce(array_agg(distinct "invoices_paid"."id"::text), \'{}\')'), '@>', DB::expr('coalesce(array_agg(distinct "invoices"."id"::text), \'{}\')'))
