bc6e6097e7132cb6b154ddf25f4697f1ed074bd0
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2014-05-26 17:46:31 +0000

    more fixes

---
+++ application/classes/formo/driver/multiselect.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Formo_Driver_Multiselect extends Formo_Driver {
+
+	protected $_view_file = 'select';
+
+  
+	public function html()
+	{
+		$this->_view
+			->set_var('tag', 'select')
+			->attr('name', $this->name().'[]');
+	}
+
+}
