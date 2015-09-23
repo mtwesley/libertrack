68691c54b464177da175639213502901d3bf3b84
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2015-09-23 14:14:41 +0000

    changes to prevent skipped numbering of export documents

--- application/classes/controller/exports.php
+++ application/classes/controller/exports.php
@@ -946,8 +946,13 @@ VALIDATION: $secret";
         
         $document->operator = $operator;
         $document->type     = $document_type;
-        $document->is_draft = $is_draft ? TRUE : FALSE;
-        $document->number   = $is_draft ? NULL : $document::create_document_number($document_type);
+        if ($document->is_draft) {
+            $document->is_draft = TRUE;
+            $document->number   = NULL;
+        } else {
+            $document->is_draft = FALSE;
+            $document->number   = $document::create_document_number($document_type);
+        }
         $document->values   = (array) $values;
         $document->created_date = SGS::date($created, SGS::PGSQL_DATE_FORMAT, TRUE);
 
@@ -1022,9 +1027,14 @@ VALIDATION: $secret";
 
           case 'final':
             set_time_limit(1800);
-            $document->is_draft = $is_draft ? TRUE : FALSE;
-            $document->number   = $is_draft ? NULL : $document::create_document_number($document_type);
-
+            if ($document->is_draft) {
+                $document->is_draft = TRUE;
+                $document->number   = NULL;
+            } else {
+                $document->is_draft = FALSE;
+                $document->number   = $document::create_document_number($document_type);
+            }
+            
             $func = strtolower('generate_'.$document_type.'_document');
             $document->file_id = self::$func($document, $ids);
 
